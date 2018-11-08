b581d6eccf37b917f892e2ceca62fae00cdfb130
Atamert Ölçgen <muhuk@muhuk.com>
2018-11-08 21:38:28 +0800

    Move stage rendering into a function

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,19 @@ const SCREEN_HEIGHT_CHAR: i32 = 50;
 const FONT_FILE: &str = "assets/terminal16x16_gs_ro.png";
 const FPS: i32 = 30;
 
+fn draw_hello_world<T: Console>(con: &mut T) {
+    con.clear();
+    con.set_char(SCREEN_WIDTH_CHAR / 2, SCREEN_HEIGHT_CHAR / 2 - 2, '');
+    con.set_alignment(console::TextAlignment::Center);
+    con.print_rect(
+        SCREEN_WIDTH_CHAR / 2,
+        SCREEN_HEIGHT_CHAR / 2 + 2,
+        SCREEN_WIDTH_CHAR,
+        1,
+        "Hello, World!",
+    );
+}
+
 fn main() {
     let mut root = console::Root::initializer()
         .title("z-buffer")
@@ -17,16 +30,7 @@ fn main() {
     tcod::system::set_fps(FPS);
 
     while !root.window_closed() {
-        root.clear();
-        root.set_char(SCREEN_WIDTH_CHAR / 2, SCREEN_HEIGHT_CHAR / 2 - 2, '');
-        root.set_alignment(console::TextAlignment::Center);
-        root.print_rect(
-            SCREEN_WIDTH_CHAR / 2,
-            SCREEN_HEIGHT_CHAR / 2 + 2,
-            SCREEN_WIDTH_CHAR,
-            1,
-            "Hello, World!",
-        );
+        draw_hello_world(&mut root);
         root.flush();
         root.wait_for_keypress(true);
     }
