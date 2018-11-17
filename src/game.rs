f57584356c815c8b908d7d4c1f1c66257fbb0380
Atamert Ölçgen <muhuk@muhuk.com>
2018-11-17 13:15:00 +0800

    Actually measure elapsed time

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,6 @@
 use stage::{Stage, StageTransition};
+use std::time::Duration;
+use tcod::system::get_elapsed_time;
 use ui::{self, UI};
 
 /// Game state.
@@ -21,8 +23,7 @@ impl Game {
 
     pub fn main_loop(&mut self) {
         while !self.ui.root_console.window_closed() {
-            self.dt = 10;
-            self.time += self.dt;
+            self.update_time();
             match self.stage.tick(self.dt) {
                 StageTransition::Continue => (),
                 StageTransition::SwitchTo(_) => unimplemented!(),
@@ -31,5 +32,15 @@ impl Game {
             self.ui.root_console.flush();
             self.ui.root_console.wait_for_keypress(true);
         }
+    }
+
+    fn update_time(&mut self) {
+        let old_time = self.time;
+        self.time = Self::to_millis(&get_elapsed_time());
+        self.dt = self.time - old_time;
+    }
+
+    fn to_millis(t: &Duration) -> u64 {
+        t.as_secs() * 1000 + t.subsec_millis() as u64
     }
 }
