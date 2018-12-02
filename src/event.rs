6bcfdd67aeb1d7da3f56ca4a7cd73e90a71277c1
Atamert Ölçgen <muhuk@muhuk.com>
2018-12-02 21:04:54 +0800

    Wrap TCOD events

--- src/event.rs
+++ src/event.rs
@@ -1,14 +1,42 @@
 use std::collections::VecDeque;
-use tcod::input::{self as tcod_input, Event as TcodEvent};
+use tcod::input::{self as tcod_input, Event as TcodEvent, KeyCode};
 
-pub type Event = TcodEvent;
+#[derive(Debug)]
+pub struct Modifiers {
+    pub shift: bool,
+    pub alt: bool,
+    pub ctrl: bool,
+}
+
+#[derive(Debug)]
+pub enum Event {
+    // KeyPress(KeyCode, Modifiers),
+    KeyDown(KeyCode, Modifiers),
+    KeyUp(KeyCode, Modifiers),
+    Mouse,
+}
 
 const DEFAULT_EVENT_QUEUE_SIZE: usize = 20;
 
 pub fn read_events() -> VecDeque<Event> {
     let mut events = VecDeque::with_capacity(DEFAULT_EVENT_QUEUE_SIZE);
     for (_, e) in tcod_input::events() {
-        events.push_back(e);
+        match e {
+            TcodEvent::Key(k) => {
+                let modifiers = Modifiers {
+                    shift: k.shift,
+                    alt: k.alt,
+                    ctrl: k.ctrl,
+                };
+                let e = if k.pressed {
+                    Event::KeyDown(k.code, modifiers)
+                } else {
+                    Event::KeyUp(k.code, modifiers)
+                };
+                events.push_back(e)
+            }
+            TcodEvent::Mouse(_) => events.push_back(Event::Mouse),
+        }
     }
     events
 }
