2ddaea2b623f5c1afbafe1aebcb0d6326c37840b
Atamert Ölçgen <muhuk@muhuk.com>
2018-12-07 23:23:37 +0800

    Refactor Input.events() & use BTreeSet instead of BTreeMap

--- src/input.rs
+++ src/input.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 use std::collections::VecDeque;
 use tcod::input::{self as tcod_input, Event as TcodEvent, KeyCode};
 
@@ -23,13 +23,13 @@ pub struct Input {
     // TODO: Key type should be a KeyCode, but since it
     //       does not satisfy the constrains we will be
     //       casting to isize.
-    key_states: BTreeMap<isize, bool>,
+    key_states: BTreeSet<isize>,
 }
 
 impl Input {
     pub fn new() -> Input {
         Input {
-            key_states: BTreeMap::new(),
+            key_states: BTreeSet::new(),
         }
     }
 
@@ -37,25 +37,12 @@ impl Input {
         let mut events = VecDeque::with_capacity(DEFAULT_EVENT_QUEUE_SIZE);
         for (_, e) in tcod_input::events() {
             match e {
-                TcodEvent::Key(k) => {
-                    let modifiers = Modifiers {
-                        shift: k.shift,
-                        alt: k.alt,
-                        ctrl: k.ctrl,
-                    };
-                    let character: Option<char> = if k.code == KeyCode::Char {
-                        Some(k.printable)
-                    } else {
-                        None
-                    };
-                    let e = if k.pressed {
-                        Event::KeyDown(k.code, character, modifiers)
-                    } else {
-                        Event::KeyUp(k.code, character, modifiers)
-                    };
-                    let e2_option = self.detect_keypress(&e);
-                    events.push_back(e);
-                    e2_option.map(|e| {
+                TcodEvent::Key(_) => {
+                    let key_up_or_key_down: Event = Self::convert_key_event(&e);
+                    let maybe_key_press = self.detect_keypress(&key_up_or_key_down);
+                    // Emit up|down event before press event.
+                    events.push_back(key_up_or_key_down);
+                    maybe_key_press.map(|e| {
                         events.push_back(e);
                     });
                 }
@@ -71,24 +58,54 @@ impl Input {
             Event::KeyUp(key_code, ..) => key_code,
             _ => panic!(),
         };
-        let key_state: bool = self
-            .key_states
-            .get(&(*key_code as isize))
-            .unwrap_or(&false)
-            .clone();
-        match (e, key_state) {
+        let code_isize: isize = *key_code as isize;
+        match (e, self.key_states.contains(&code_isize)) {
             (Event::KeyDown(_, character, modifiers), true) => {
                 Some(Event::KeyPress(*key_code, *character, (*modifiers).clone()))
             }
             (Event::KeyDown(..), false) => {
-                self.key_states.insert(*key_code as isize, true);
+                self.key_states.insert(code_isize);
                 None
             }
             (Event::KeyUp(_, character, modifiers), _) => {
-                self.key_states.remove(&(*key_code as isize));
+                self.key_states.remove(&code_isize);
                 Some(Event::KeyPress(*key_code, *character, (*modifiers).clone()))
             }
             _ => None,
+        }
+    }
+
+    #[inline]
+    fn convert_key_event(event: &TcodEvent) -> Event {
+        match event {
+            TcodEvent::Key(k) => {
+                let modifiers = Modifiers {
+                    shift: k.shift,
+                    alt: k.alt,
+                    ctrl: k.ctrl,
+                };
+                let character: Option<char> = Self::character_for(&event);
+                if k.pressed {
+                    Event::KeyDown(k.code, character, modifiers)
+                } else {
+                    Event::KeyUp(k.code, character, modifiers)
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    #[inline]
+    fn character_for(event: &TcodEvent) -> Option<char> {
+        match event {
+            TcodEvent::Key(k) => {
+                if k.code == KeyCode::Char {
+                    Some(k.printable)
+                } else {
+                    None
+                }
+            }
+            _ => unreachable!(),
         }
     }
 }
