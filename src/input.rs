97d1339f28630e4a47e0dc3793cce4f674d78763
Atamert Ölçgen <muhuk@muhuk.com>
2018-12-07 22:29:46 +0800

    Emit KeyPress by combining KeyUp & KeyDown appropriately

--- src/input.rs
+++ src/input.rs
@@ -1,31 +1,39 @@
+use std::collections::BTreeMap;
 use std::collections::VecDeque;
 use tcod::input::{self as tcod_input, Event as TcodEvent, KeyCode};
 
 const DEFAULT_EVENT_QUEUE_SIZE: usize = 20;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Modifiers {
     pub shift: bool,
     pub alt: bool,
     pub ctrl: bool,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Event {
-    // KeyPress(KeyCode, Modifiers),
+    KeyPress(KeyCode, Modifiers),
     KeyDown(KeyCode, Modifiers),
     KeyUp(KeyCode, Modifiers),
     Mouse,
 }
 
-pub struct Input {}
+pub struct Input {
+    // TODO: Key type should be a KeyCode, but since it
+    //       does not satisfy the constrains we will be
+    //       casting to isize.
+    key_states: BTreeMap<isize, bool>,
+}
 
 impl Input {
     pub fn new() -> Input {
-        Input {}
+        Input {
+            key_states: BTreeMap::new(),
+        }
     }
 
-    pub fn events(&self) -> VecDeque<Event> {
+    pub fn events(&mut self) -> VecDeque<Event> {
         let mut events = VecDeque::with_capacity(DEFAULT_EVENT_QUEUE_SIZE);
         for (_, e) in tcod_input::events() {
             match e {
@@ -40,11 +48,42 @@ impl Input {
                     } else {
                         Event::KeyUp(k.code, modifiers)
                     };
-                    events.push_back(e)
+                    let e2_option = self.detect_keypress(&e);
+                    events.push_back(e);
+                    e2_option.map(|e| {
+                        events.push_back(e);
+                    });
                 }
                 TcodEvent::Mouse(_) => events.push_back(Event::Mouse),
             }
         }
         events
+    }
+
+    fn detect_keypress(&mut self, e: &Event) -> Option<Event> {
+        let key_code: &KeyCode = match e {
+            Event::KeyDown(key_code, _) => key_code,
+            Event::KeyUp(key_code, _) => key_code,
+            _ => panic!(),
+        };
+        let key_state: bool = self
+            .key_states
+            .get(&(*key_code as isize))
+            .unwrap_or(&false)
+            .clone();
+        match (e, key_state) {
+            (Event::KeyDown(_, modifiers), true) => {
+                Some(Event::KeyPress(*key_code, (*modifiers).clone()))
+            }
+            (Event::KeyDown(_, _), false) => {
+                self.key_states.insert(*key_code as isize, true);
+                None
+            }
+            (Event::KeyUp(_, modifiers), _) => {
+                self.key_states.remove(&(*key_code as isize));
+                Some(Event::KeyPress(*key_code, (*modifiers).clone()))
+            }
+            _ => None,
+        }
     }
 }
