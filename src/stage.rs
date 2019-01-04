eec9eea6d99ea98b33c33c909e90a9812c548655
Atamert Ölçgen <muhuk@muhuk.com>
2019-01-04 21:21:16 +0800

    Exit game when Exit is chosen in main menu

--- src/stage.rs
+++ src/stage.rs
@@ -1,6 +1,7 @@
 use crate::input::{Event, EventIterator, KeyCode};
 use crate::menu::Menu;
-use crate::stage::main_menu::MainMenu;
+use crate::stage::main_menu::{Choice, MainMenu};
+use std::process::exit;
 
 pub mod main_menu;
 
@@ -21,14 +22,23 @@ impl Stage {
 
     fn tick_main_menu(&mut self, _dt_millis: u32, events: EventIterator) -> StageTransition {
         let Stage::MainMenu(m) = self;
+        let transition = StageTransition::Continue;
+        let exit_code_ok: i32 = 0;
         for e in events {
             match e {
                 Event::KeyPress(KeyCode::Up, ..) => m.select_previous(),
                 Event::KeyPress(KeyCode::Down, ..) => m.select_next(),
+                Event::KeyPress(KeyCode::Enter, ..) => {
+                    println!("Main menu - chosen {}", &m.selected);
+                    if m.is_selected(&Choice::Exit) {
+                        println!("Bye!");
+                        exit(exit_code_ok);
+                    }
+                }
                 _ => (),
             }
         }
-        StageTransition::Continue
+        transition
     }
 }
 
