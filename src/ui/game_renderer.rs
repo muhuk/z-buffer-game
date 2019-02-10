cd3a5997c50a08c054094edf6d8cacf7100e84cb
Atamert Ölçgen <muhuk@muhuk.com>
2019-02-10 11:27:40 +0800

    Take bottom panel into consideration when calculating map viewport

--- src/ui/game_renderer.rs
+++ src/ui/game_renderer.rs
@@ -1,7 +1,8 @@
 use crate::stage::game::Game;
-use crate::ui::constants::{MAP_MIN_SIZE, SIDE_PANEL_WIDTH};
+use crate::ui::constants::{
+    BOTTOM_PANEL_HEIGHT, MAP_MIN_SIZE, SIDE_PANEL_WIDTH,
+};
 use crate::ui::render::Render;
-use std::cmp::max;
 use std::fmt;
 use tcod::console::{blit, BackgroundFlag, Console, Offscreen, TextAlignment};
 
@@ -13,7 +14,8 @@ pub struct GameRenderer {
 impl GameRenderer {
     pub fn new(width: u32, height: u32) -> GameRenderer {
         let root = Offscreen::new(width as i32, height as i32);
-        let (map_w, map_h) = Self::calculate_map_viewport((width, height)).unwrap();
+        let (map_w, map_h) =
+            Self::calculate_map_viewport((width, height)).unwrap();
         let map = Offscreen::new(map_w as i32, map_h as i32);
         GameRenderer { root, map }
     }
@@ -24,11 +26,15 @@ impl GameRenderer {
         blit(&self.map, (0, 0), (w, h), &mut self.root, (0, 0), 1.0, 1.0);
     }
 
-    pub(self) fn calculate_map_viewport(requested_size: (u32, u32)) -> Option<(u32, u32)> {
+    pub(self) fn calculate_map_viewport(
+        requested_size: (u32, u32),
+    ) -> Option<(u32, u32)> {
         let (req_w, req_h) = requested_size;
         let (map_min_w, map_min_h) = MAP_MIN_SIZE;
-        if req_w >= map_min_w + SIDE_PANEL_WIDTH && req_h >= map_min_h {
-            Some((req_w - SIDE_PANEL_WIDTH, req_h))
+        if (req_w >= map_min_w + SIDE_PANEL_WIDTH)
+            && (req_h >= map_min_h + BOTTOM_PANEL_HEIGHT)
+        {
+            Some((req_w - SIDE_PANEL_WIDTH, req_h - BOTTOM_PANEL_HEIGHT))
         } else {
             None
         }
@@ -69,7 +75,7 @@ impl Render for GameRenderer {
         map.set_alignment(TextAlignment::Center);
         map.print_rect(
             map.width() / 2,
-            map.height() / 2 + 2,
+            map.height() / 2,
             map.width(),
             1,
             "Game Stage",
@@ -90,7 +96,7 @@ mod tests {
     fn viewport_calculation_is_never_less_than_minimum_requirements() {
         let (map_min_width, map_min_height) = MAP_MIN_SIZE;
         let min_width = map_min_width + SIDE_PANEL_WIDTH;
-        let min_height = map_min_height;
+        let min_height = map_min_height + BOTTOM_PANEL_HEIGHT;
 
         for b in 0..MAX_Y {
             for a in 0..MAX_X {
@@ -107,7 +113,7 @@ mod tests {
                             "Map height {} is calculated as less than minimum required!",
                             h
                         );
-                        assert_eq!(b, h);
+                        assert_eq!(b, h + BOTTOM_PANEL_HEIGHT);
                     }
                     None => assert!(a < min_width || b < min_height),
                 }
