be4fcad02b01c3abeb8f372c3ac1a08429d94010
Atamert Ölçgen <muhuk@muhuk.com>
2019-02-17 13:02:32 +0800

    Rename calculate_map_viewport fn

--- src/ui/game_renderer.rs
+++ src/ui/game_renderer.rs
@@ -15,18 +15,12 @@ impl GameRenderer {
     pub fn new(width: u32, height: u32) -> GameRenderer {
         let root = Offscreen::new(width as i32, height as i32);
         let (map_w, map_h) =
-            Self::calculate_map_viewport((width, height)).unwrap();
+            Self::calculate_map_viewport_size((width, height)).unwrap();
         let map = Offscreen::new(map_w as i32, map_h as i32);
         GameRenderer { root, map }
     }
 
-    fn blit(&mut self) {
-        let w = self.map.width();
-        let h = self.map.height();
-        blit(&self.map, (0, 0), (w, h), &mut self.root, (0, 0), 1.0, 1.0);
-    }
-
-    pub(self) fn calculate_map_viewport(
+    pub(self) fn calculate_map_viewport_size(
         requested_size: (u32, u32),
     ) -> Option<(u32, u32)> {
         let (req_w, req_h) = requested_size;
@@ -38,6 +32,12 @@ impl GameRenderer {
         } else {
             None
         }
+    }
+
+    fn blit(&mut self) {
+        let w = self.map.width();
+        let h = self.map.height();
+        blit(&self.map, (0, 0), (w, h), &mut self.root, (0, 0), 1.0, 1.0);
     }
 }
 
@@ -57,7 +57,7 @@ impl Render for GameRenderer {
     fn update(&mut self, stage: &Game) {
         // TODO: Remove notes below after implementation.
         //
-        // 1. Figure out the camera location.  It comes from the stage.
+        // 1. [Done] Figure out the camera location.  It comes from the stage.
         // 2. Based on map size, figure out the viewport in world coordinates.
         // 3. Query the map in stage for tile types.
         // 4. Convert tiles to glyphs and render them on map.
@@ -96,14 +96,14 @@ mod tests {
     const MAX_Y: u32 = 2160 / 8 * 2;
 
     #[test]
-    fn viewport_calculation_is_never_less_than_minimum_requirements() {
+    fn map_dimensions_are_never_less_than_minimum_requirements() {
         let (map_min_width, map_min_height) = MAP_MIN_SIZE;
         let min_width = map_min_width + SIDE_PANEL_WIDTH;
         let min_height = map_min_height + BOTTOM_PANEL_HEIGHT;
 
         for b in 0..MAX_Y {
             for a in 0..MAX_X {
-                match GameRenderer::calculate_map_viewport((a, b)) {
+                match GameRenderer::calculate_map_viewport_size((a, b)) {
                     Some((w, h)) => {
                         assert!(
                             w >= map_min_width,
