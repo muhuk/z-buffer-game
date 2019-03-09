db725371ef209436001add4f30b2bba747c92bb0
Atamert Ölçgen <muhuk@muhuk.com>
2019-03-09 15:12:10 +0800

    Read tile data from assets

--- src/asset.rs
+++ src/asset.rs
@@ -6,19 +6,27 @@ use std::path::Path;
 use std::sync::Once;
 
 const TERMINAL16X16_GS_RO_PATH: &str = "terminal16x16_gs_ro.png";
-static TERMINAL16X16_GS_RO_DATA: &[u8] = include_bytes!("../assets/terminal16x16_gs_ro.png");
+static TERMINAL16X16_GS_RO_DATA: &[u8] =
+    include_bytes!("../assets/terminal16x16_gs_ro.png");
+
+const TILES_TOML_PATH: &str = "tiles.toml";
+static TILES_TOML_DATA: &[u8] = include_bytes!("../assets/tiles.toml");
 
 static INIT: Once = Once::new();
 
 pub enum Assets {
     FontTerminal16x16GsRo,
+    TilesToml,
 }
 
 impl Assets {
     pub fn extract(&self) -> Result<Box<Path>> {
         initialize();
         let (file_name, data) = match self {
-            Assets::FontTerminal16x16GsRo => (TERMINAL16X16_GS_RO_PATH, TERMINAL16X16_GS_RO_DATA),
+            Assets::FontTerminal16x16GsRo => {
+                (TERMINAL16X16_GS_RO_PATH, TERMINAL16X16_GS_RO_DATA)
+            }
+            Assets::TilesToml => (TILES_TOML_PATH, TILES_TOML_DATA),
         };
         let mut path = conf::assets_directory();
         path.push(&file_name);
--- src/ui/tiles.rs
+++ src/ui/tiles.rs
@@ -1,12 +1,10 @@
+use crate::asset::Assets;
 use serde::Deserialize;
 use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
 use std::result::Result;
 use toml;
-
-#[derive(Debug, Deserialize)]
-struct Tiles {
-    tiles: Vec<Tile>,
-}
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
 struct Tile {
@@ -14,19 +12,34 @@ struct Tile {
     glyph_id: u16,
 }
 
-impl Tile {
-    pub fn read(s: &str) -> Result<HashMap<String, Tile>, String> {
+#[derive(Debug, Deserialize)]
+struct Tiles {
+    tiles: Vec<Tile>,
+}
+
+impl Tiles {
+    pub fn read() -> HashMap<String, Tile> {
+        Self::from_path(&*Assets::TilesToml.extract().unwrap()).unwrap()
+    }
+
+    pub(self) fn from_str(s: &str) -> Result<HashMap<String, Tile>, String> {
+        let conf: Tiles = toml::from_str::<Tiles>(s)
+            .map_err(|e| format!("Failed to parse tiles: {:?}", e))?;
+
         let mut result: HashMap<String, Tile> = HashMap::new();
-        // TODO: Process error from from_str
-        let deserialized = toml::from_str::<Tiles>(s).unwrap();
-        deserialized
-            .tiles
+        conf.tiles
             .iter()
             .map(|t| (t.name.clone(), t))
             .for_each(|(k, v)| {
                 result.insert(k, v.clone());
             });
         Result::Ok(result)
+    }
+
+    fn from_path(path: &Path) -> Result<HashMap<String, Tile>, String> {
+        Self::from_str(&fs::read_to_string(path).map_err(|e| {
+            format!("Failed to read {}, {:?}", path.to_str().unwrap(), e)
+        })?)
     }
 }
 
@@ -42,7 +55,8 @@ mod tests {
             glyph_id = 0x0001
         "#;
 
-        let deserialized: HashMap<String, Tile> = Tile::read(input).unwrap();
+        let deserialized: HashMap<String, Tile> =
+            Tiles::from_str(input).unwrap();
         assert_eq!(
             vec!["Test"],
             deserialized.keys().collect::<Vec<&String>>()
