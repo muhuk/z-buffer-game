da0da7810ad6cab13f4e226581f7833b51063b31
Atamert Ölçgen <muhuk@muhuk.com>
2019-04-29 21:17:38 +0800

    Add Time resource

--- src/game.rs
+++ src/game.rs
@@ -3,9 +3,11 @@ pub use self::game_event::GameEvent;
 pub use self::input::InputSystem;
 pub use self::log::{GameLog, LogEntry};
 pub use self::rendering::RenderingSystem;
+pub use self::time::Time;
 
 mod cursor;
 mod game_event;
 mod input;
 mod log;
 mod rendering;
+mod time;
---
+++ src/game/time.rs
@@ -0,0 +1,39 @@
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Time {
+    dt_millis: u32,
+    t_millis: u64,
+}
+
+impl Time {
+    pub fn advance_dt(&mut self, dt: u32) {
+        self.dt_millis = dt;
+        self.t_millis += dt as u64;
+    }
+
+    pub fn dt_millis(self) -> u32 {
+        self.dt_millis
+    }
+
+    pub fn t_millis(self) -> u64 {
+        self.t_millis
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn advance_dt_increments_both_dt_and_t() {
+        let mut time = Time::default();
+        time.advance_dt(1000);
+        assert_eq!(1000, time.dt_millis());
+        assert_eq!(1000, time.t_millis());
+        time.advance_dt(2000);
+        assert_eq!(2000, time.dt_millis());
+        assert_eq!(3000, time.t_millis());
+        time.advance_dt(250);
+        assert_eq!(250, time.dt_millis());
+        assert_eq!(3250, time.t_millis());
+    }
+}
